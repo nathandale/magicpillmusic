4b75772b1eadd11b70e8f31ce291091ced82eaf9
Nathan Dale <admin@magicpillmusic.com>
2026-03-07 04:14:08 +0000

    Initial Payload install

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260307_033448 from './20260307_033448';
+
+export const migrations = [
+  {
+    up: migration_20260307_033448.up,
+    down: migration_20260307_033448.down,
+    name: '20260307_033448'
+  },
+];
